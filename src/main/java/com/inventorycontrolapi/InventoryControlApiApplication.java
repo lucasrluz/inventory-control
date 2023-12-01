17f8c20c8b89bc343257c06b19adc84b820d7db9
lucasrluz <lucasr.luz42@gmail.com>
2023-12-01 13:55:10 -0300

    feat: primeiro commit

---
+++ src/main/java/com/inventorycontrolapi/InventoryControlApiApplication.java
@@ -0,0 +1,13 @@
+package com.inventorycontrolapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryControlApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryControlApiApplication.class, args);
+	}
+
+}
