9af4e3d9df56bbcedfafaa10295d8bf09dae248b
lucasrluz <lucasr.luz42@gmail.com>
2023-12-04 14:31:39 -0300

    feat: adicionado rota/método update no CompanyController

--- src/main/java/com/inventorycontrolapi/controllers/CompanyController.java
+++ src/main/java/com/inventorycontrolapi/controllers/CompanyController.java
@@ -5,6 +5,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +17,8 @@ import com.inventorycontrolapi.dtos.company.SignInCompanyDTORequest;
 import com.inventorycontrolapi.dtos.company.SignInCompanyDTOResponse;
 import com.inventorycontrolapi.dtos.company.SignUpCompanyDTORequest;
 import com.inventorycontrolapi.dtos.company.SignUpCompanyDTOResponse;
+import com.inventorycontrolapi.dtos.company.UpdateCompanyDTORequest;
+import com.inventorycontrolapi.dtos.company.UpdateCompanyDTOResponse;
 import com.inventorycontrolapi.services.CompanyService;
 
 @RestController
@@ -59,5 +62,20 @@ public class CompanyController extends ResponseEntityExceptionHandler {
 		GetCompanyDTOResponse getCompanyDTOResponse= this.companyService.get(getCompanyDTORequest);
 
 		return ResponseEntity.status(200).body(getCompanyDTOResponse);
+	}
+
+	@PutMapping
+	public ResponseEntity<Object> update(@RequestBody UpdateCompanyDTORequest updateCompanyDTORequest, Authentication authentication) {
+		try {
+			String companyId = authentication.getName();
+
+			updateCompanyDTORequest.setCompanyId(companyId);
+
+			UpdateCompanyDTOResponse updateCompanyDTOResponse = this.companyService.update(updateCompanyDTORequest);
+
+			return ResponseEntity.status(201).body(updateCompanyDTOResponse);
+		} catch (Exception exception) {
+			return ResponseEntity.status(400).body(exception.getMessage());
+		}
 	}
 }
