3244dd0698d672da879f58a102d47a079bd8e822
lucasrluz <lucasr.luz42@gmail.com>
2023-12-06 15:13:58 -0300

    feat: adicionado método no ItemCategoryRepository findByCompanyModel

--- src/main/java/com/inventorycontrolapi/repositories/ItemCategoryRepository.java
+++ src/main/java/com/inventorycontrolapi/repositories/ItemCategoryRepository.java
@@ -1,11 +1,14 @@
 package com.inventorycontrolapi.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.inventorycontrolapi.models.CompanyModel;
 import com.inventorycontrolapi.models.ItemCategoryModel;
 
 @Repository
 public interface ItemCategoryRepository extends JpaRepository<ItemCategoryModel, Long> {
-
+	List<ItemCategoryModel> findByCompanyModel(CompanyModel companyModel);
 }
