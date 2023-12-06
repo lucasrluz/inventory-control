74527058ea51c8bf18e50f6f00ad90dcb432ee2d
lucasrluz <lucasr.luz42@gmail.com>
2023-12-05 21:57:26 -0300

    feat: adicionado InvalidItemCategoryDomainException

---
+++ src/main/java/com/inventorycontrolapi/domains/exceptions/InvalidItemCategoryDomainException.java
@@ -0,0 +1,7 @@
+package com.inventorycontrolapi.domains.exceptions;
+
+public class InvalidItemCategoryDomainException extends Exception {
+	public InvalidItemCategoryDomainException(String message) {
+		super(message);
+	}
+}
