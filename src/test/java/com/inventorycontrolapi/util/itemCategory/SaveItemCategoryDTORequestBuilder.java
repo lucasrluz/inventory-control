e7b7dee1400968af3bd64455519987121046e5a7
lucasrluz <lucasr.luz42@gmail.com>
2023-12-06 14:07:35 -0300

    feat: adicionado novo método no SaveItemCategoryDTORequestBuilder para criar com nome vazio

--- src/test/java/com/inventorycontrolapi/util/itemCategory/SaveItemCategoryDTORequestBuilder.java
+++ src/test/java/com/inventorycontrolapi/util/itemCategory/SaveItemCategoryDTORequestBuilder.java
@@ -6,4 +6,8 @@ public abstract class SaveItemCategoryDTORequestBuilder {
 	public static SaveItemCategoryDTORequest createWithValidData() {
 		return new SaveItemCategoryDTORequest("Item Category A", "0");
 	}
+
+	public static SaveItemCategoryDTORequest createWithEmptyName() {
+		return new SaveItemCategoryDTORequest("", "0");
+	}
 }
