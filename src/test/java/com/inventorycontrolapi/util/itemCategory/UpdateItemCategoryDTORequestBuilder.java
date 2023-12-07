12440547a1a452f4c7a727efe0dfdccdbef1ac91
lucasrluz <lucasr.luz42@gmail.com>
2023-12-07 15:49:27 -0300

    feat: adicionado UpdateItemCategoryDTORequestBuilder

---
+++ src/test/java/com/inventorycontrolapi/util/itemCategory/UpdateItemCategoryDTORequestBuilder.java
@@ -0,0 +1,17 @@
+package com.inventorycontrolapi.util.itemCategory;
+
+import com.inventorycontrolapi.dtos.itemCategory.UpdateItemCategoryDTORequest;
+
+public abstract class UpdateItemCategoryDTORequestBuilder {
+	public static UpdateItemCategoryDTORequest createWithValidData() {
+		return new UpdateItemCategoryDTORequest("0", "0", "Item Category B");
+	}
+
+	public static UpdateItemCategoryDTORequest createWithValidDataAndInvalidCompanyId() {
+		return new UpdateItemCategoryDTORequest("0", "1", "Item Category B");
+	}
+	
+	public static UpdateItemCategoryDTORequest createWithEmptyName() {
+		return new UpdateItemCategoryDTORequest("0", "0", "Item Category B");
+	}
+}
