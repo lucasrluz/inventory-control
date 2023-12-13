24f3970c8bb93a2f07f696d9acffd77e545c9e9a
lucasrluz <lucasr.luz42@gmail.com>
2023-12-13 15:07:46 -0300

    feat: adicionado novo método no SaveItemDTORequestBuilder para criar com nome vazio

--- src/test/java/com/inventorycontrolapi/util/item/SaveItemDTORequestBuilder.java
+++ src/test/java/com/inventorycontrolapi/util/item/SaveItemDTORequestBuilder.java
@@ -6,4 +6,8 @@ public abstract class SaveItemDTORequestBuilder {
 	public static SaveItemDTORequest createWithValidData() {
 		return new SaveItemDTORequest("0", "0", "Item A", "1.25", "1");
 	}
+	
+	public static SaveItemDTORequest createWithEmptyName() {
+		return new SaveItemDTORequest("0", "0", "", "1.25", "1");
+	}
 }
